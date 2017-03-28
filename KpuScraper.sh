c338659d18fcdaf4247c15f1a2062709125bc7d6
Keenan mandela Gebze <keenan.gebze@gmail.com>
2017-03-28 21:36:34 +0700

    removing $WORKSPACE: using relative path. The program can now be run in its directory using ./KpuScraper.sh

--- KpuScraper.sh
+++ KpuScraper.sh
@@ -1,5 +1,4 @@
 #Change with the path to pilkada-2017-scraper.
-WORKSPACE=~/workspace/pilkada-2017-scraper
 
 OUTPUT_DIR=output
 VILLAGE_LIST=output/list_kelurahan.txt
@@ -7,20 +6,18 @@ SUMMARY=output/pilkada_gabung_ringkas2.txt
 RESULT_TPS=output/hasil_TPS.csv
 RESULT_KEL=output/hasil_KELURAHAN.csv
 
-cd $WORKSPACE
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 
 #Download the data for DKI Jakarta (you can change it into any province you want, if any)
-cd $WORKSPACE
 wget -r --no-parent https://pilkada2017.kpu.go.id/hasil/t1/dki_jakarta
 
 #List all of the villages
-cd $WORKSPACE/pilkada2017.kpu.go.id/hasil/t1
-find */*/*/* > $WORKSPACE/$VILLAGE_LIST
+cd pilkada2017.kpu.go.id/hasil/t1
+find */*/*/* > ../../../$VILLAGE_LIST
 
 #Initial scrape (before using the java program)
-cd $WORKSPACE
+cd ../../../
 for i in $(cat $VILLAGE_LIST); 
 do
   echo "REGION=$i" >> $SUMMARY;
@@ -34,4 +31,5 @@ done
 javac KpuScraper.java
 java KpuScraper $SUMMARY > $RESULT_TPS
 
+#agregates the result.
 python Agregator.py $RESULT_TPS > $RESULT_KEL
